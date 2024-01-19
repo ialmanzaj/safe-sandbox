a0e43b4ce4dbba939dfb8d4412c16cae8fbda39b
Ruslan Serebriakov <rsrbk1@gmail.com>
2024-01-19 20:57:19 +0000

    remove mumbai

--- src/domains/Network/api/networks.ts
+++ src/domains/Network/api/networks.ts
@@ -2,44 +2,22 @@ import { Network } from "../Network";
 
 export const networks: Network[] = [
   {
-    name: "Polygon Mumbai",
-    testnet: true,
-    icon: "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/info/logo.png",
-    bundlerUrl: process.env.NEXT_PUBLIC_BUNDLER_URL_MUMBAI!,
-    rpcUrl: "https://rpc.ankr.com/polygon_mumbai",
-    zeroExBaseUrl: "",
-    id: 80001,
-    network: "Polygon Mumbai",
-    queryName: "mumbai",
-    nativeCurrency: { name: "Polygon MATIC", symbol: "MATIC", decimals: 18 },
-    rpcUrls: {
-      default: {
-        http: ["https://rpc.ankr.com/polygon_mumbai"],
-      },
-      public: {
-        http: ["https://rpc.ankr.com/polygon_mumbai"],
-      },
-    },
-    blockExplorerUrl: "https://mumbai.polygonscan.com",
-    explorerName: "PolygonScan",
-  },
-  {
     name: "Ethereum Sepolia",
     testnet: true,
     icon: "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/info/logo.png",
-    bundlerUrl: process.env.NEXT_PUBLIC_BUNDLER_URL_MUMBAI!,
-    rpcUrl: "https://rpc.ankr.com/eth_sepolia",
+    bundlerUrl: process.env.NEXT_PUBLIC_BUNDLER_URL_SEPOLIA!,
+    rpcUrl: "https://rpc.sepolia.org",
     zeroExBaseUrl: "",
     id: 11155111,
     network: "Ethereum Sepolia",
     queryName: "sepolia",
-    nativeCurrency: { name: "SepoliaETH", symbol: "SepoliaETH", decimals: 18 },
+    nativeCurrency: { name: "Sepolia ETH", symbol: "ETH", decimals: 18 },
     rpcUrls: {
       default: {
-        http: ["https://rpc.ankr.com/eth_sepolia"],
+        http: ["https://rpc.sepolia.org"],
       },
       public: {
-        http: ["https://rpc.ankr.com/eth_sepolia"],
+        http: ["https://rpc.sepolia.org"],
       },
     },
     blockExplorerUrl: "https://sepolia.etherscan.io/",
--- src/domains/Token/api/data/tokens.ts
+++ src/domains/Token/api/data/tokens.ts
@@ -2,15 +2,6 @@ import { zeroAddress } from 'viem'
 
 export const allTokens = [
   {
-    name: 'MATIC',
-    token_address: 'MATIC',
-    symbol: 'MATIC',
-    decimals: 18,
-    chainId: 80001,
-    logoURI:
-      'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png',
-  },
-  {
     name: 'ETH',
     token_address: 'ETH',
     symbol: 'ETH',
@@ -19,27 +10,9 @@ export const allTokens = [
     logoURI:
       'https://assets.coingecko.com/coins/images/279/standard/ethereum.png?1696501628',
   },
-  {
-    name: 'Wrapped Matic',
-    token_address: '0x9c3c9283d3e44854697cd22d3faa240cfb032889',
-    symbol: 'WMATIC',
-    decimals: 18,
-    chainId: 80001,
-    logoURI:
-      'https://assets.coingecko.com/coins/images/4713/thumb/matic-token-icon.png?1624446912',
-  },
 ]
 
 export const nativeCoins = [
-  {
-    name: 'MATIC',
-    token_address: 'MATIC',
-    symbol: 'MATIC',
-    decimals: 18,
-    chainId: 80001,
-    logoURI:
-      'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png',
-  },
   {
     name: 'ETH',
     token_address: 'ETH',
@@ -52,24 +25,6 @@ export const nativeCoins = [
 ]
 
 export const ERC20Tokens = [
-  {
-    name: 'Wrapped Matic',
-    token_address: '0x9c3c9283d3e44854697cd22d3faa240cfb032889',
-    symbol: 'WMATIC',
-    decimals: 18,
-    chainId: 80001,
-    logoURI:
-      'https://assets.coingecko.com/coins/images/4713/thumb/matic-token-icon.png?1624446912',
-  },
-  {
-    name: 'USDC',
-    token_address: '0x9999f7Fea5938fD3b1E26A12c3f2fb024e194f97',
-    symbol: 'USDC',
-    decimals: 6,
-    chainId: 80001,
-    logoURI:
-      'https://assets.coingecko.com/coins/images/6319/standard/usdc.png?1696506694',
-  },
 ]
 
 export const AnyERC20Token = {
