804223a68f4687ab15ca594f2e80d972bf20a868
Maksym Zub <kroistsmeh@gmail.com>
2024-01-21 05:21:03 +0100

    change RPC

--- src/domains/Network/api/networks.ts
+++ src/domains/Network/api/networks.ts
@@ -6,7 +6,7 @@ export const networks: Network[] = [
     testnet: true,
     icon: "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/info/logo.png",
     bundlerUrl: process.env.NEXT_PUBLIC_BUNDLER_URL_SEPOLIA!,
-    rpcUrl: "https://rpc.sepolia.org",
+    rpcUrl: "https://rpc.ankr.com/eth_sepolia",
     zeroExBaseUrl: "",
     id: 11155111,
     network: "Ethereum Sepolia",
@@ -14,10 +14,10 @@ export const networks: Network[] = [
     nativeCurrency: { name: "Sepolia ETH", symbol: "ETH", decimals: 18 },
     rpcUrls: {
       default: {
-        http: ["https://rpc.sepolia.org"],
+        http: ["https://rpc.ankr.com/eth_sepolia"],
       },
       public: {
-        http: ["https://rpc.sepolia.org"],
+        http: ["https://rpc.ankr.com/eth_sepolia"],
       },
     },
     blockExplorerUrl: "https://sepolia.etherscan.io/",
